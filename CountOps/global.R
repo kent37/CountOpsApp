d3e2c32a0411b4412c2e4f7c616a62862d2f023f
Kent Johnson <kent3737@gmail.com>
2020-03-03 20:22:31 -0500

    Initial commit

---
+++ CountOps/global.R
@@ -0,0 +1,8 @@
+library(shiny)
+library(shinydashboard)
+library(CountOpsLogan)
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(silgelib)
+all_data = countops_hourly_jets
---
+++ CountOps/ui.R
@@ -0,0 +1,20 @@
+# CountOps UI
+dashboardPage(
+  dashboardHeader(title='Logan Airport CountOps Explorer', titleWidth=400),
+  dashboardSidebar(
+      sliderInput('year_range', 'Years', 2010, 2019, ticks=FALSE,
+                  value=c(2010, 2019), step=1, round=TRUE, sep=''),
+      sliderInput('hour_range', 'Hours', 0, 23, ticks=FALSE,
+                  value=c(5, 8), step=1, round=TRUE),
+      radioButtons('group_by', 'Summarize by', 
+                   list('Month'='Month', 'Day'='Date'), 
+                   selected='Month', inline=TRUE),
+      checkboxInput('show_smooth', 'Show linear fit?', value=TRUE)
+  ),
+  
+  dashboardBody(
+      fluidRow(
+          plotOutput('the_plot', width='95%')
+      )
+  )
+)
