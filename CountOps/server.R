22ab4dbcd5967ed6a55fadd5b6f12b14391efb4f
Kent Johnson <kent3737@gmail.com>
2020-03-04 12:26:55 -0500

    Select operation, better title

--- CountOps/server.R
+++ CountOps/server.R
@@ -1,33 +1,50 @@
 # CountOps server
 
 filter_data = function(start_year, end_year, 
-                       start_hour, end_hour, group_by_) {
+                       start_hour, end_hour, operation, group_by_) {
     group_by_ = as.character(group_by_)
     # Using base R here is orders of magnitude faster than filter
     all_data[all_data$Year >= start_year & all_data$Year <= end_year &
-             all_data$Hour >= start_hour & all_data$Hour <= end_hour,] %>% 
-        group_by(!!rlang::ensym(group_by_)) %>% 
+             all_data$Hour >= start_hour & all_data$Hour <= end_hour &
+             all_data$Operation==operation,] %>% 
+        group_by(!!ensym(group_by_)) %>% 
         summarize(Count=sum(Count))
 }
 
+as_am_pm = function(t) {
+    case_when(
+        t == 12 ~ 'noon',
+        t == 0 || t == 24 ~ 'midnight',
+        t < 12 ~ as.character(glue('{t}am')),
+        TRUE ~ as.character(glue('{t-12}pm'))
+    )
+}
+
 server <- function(input, output) {
-  output$the_plot = renderPlot({
-    df = filter_data(input$year_range[1], input$year_range[2],
+  filtered = reactive({
+      filter_data(input$year_range[1], input$year_range[2],
                      input$hour_range[1], input$hour_range[2],
-                     input$group_by)
-    
+                     input$operation, input$group_by)
+  })
+  
+  title = reactive({
     period = if_else(input$group_by=='Month', 'monthly', 'daily')
-    title=paste0('Logan Airport total ', period, ' departures, ',
-                 paste0(input$hour_range, ':00', collapse='-'))
+    operation = if_else(input$operation=='Departure', 
+                        'departures', 'arrivals')
+    glue('Logan Airport total {period} {operation}, ',
+    '{as_am_pm(input$hour_range[1])}-{as_am_pm(input$hour_range[2]+1)}')
+  })
+  
+  output$the_plot = renderPlot({
+    df = filtered()
+    
 
     p = ggplot(df, aes_string(input$group_by, 'Count')) +
       geom_point(color='steelblue') +
-      # scale_color_gradientn(colors=pals::kovesi.rainbow(100), 
-      #                       guide='none') +
       scale_x_date(date_breaks='1 year', minor_breaks=NULL,
                    date_labels='%Y', expand=expand_scale(mult=0.1)) +
       silgelib::theme_plex() +
-      labs(title=title,
+      labs(title=title(),
            x='', y='Departure count',
            caption='Data from FAA FOIA request Chart © 2020 Kent Johnson')
     if (input$show_smooth)
